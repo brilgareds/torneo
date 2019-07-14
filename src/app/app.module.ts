cd36e1de652fd12720d12104143bd2176c039d4f
Gabriel Angarita <gabriel.angarita.93@gmail.com>
2019-07-14 10:46:14 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './components/home/home.component';
+import { TournamentComponent } from './components/tournament/tournament.component';
+import { ScoresComponent } from './components/scores/scores.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    MenuComponent,
+    HomeComponent,
+    TournamentComponent,
+    ScoresComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [MenuComponent]
 })
 export class AppModule { }
